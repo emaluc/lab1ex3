62eecd25f95acb3ee295f8254e2fd63828aa636a
Emanuela Lucaci <emanuela.lucaci26@gmail.com>
2022-11-28 16:25:57 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+            tastaura */
+
+            Console.WriteLine("Acest program va afisa ultima cifra a unui numar intreg.");
+            Console.WriteLine("Introduceti numarul");
+            ulong numar = ulong.Parse(Console.ReadLine());
+
+            Console.WriteLine("Acesta este ultima cifra a numarului intreg");
+            Console.WriteLine(numar % 10);
+
+
+            
+        }
+    }
+}
